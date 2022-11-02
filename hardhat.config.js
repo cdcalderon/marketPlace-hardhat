ca7b4c6a5562295549ce14d4ca6fa345a896fff1
Carlos Calderon <cdcalderon@gmail.com>
2022-11-01 22:20:56 -0500

    fix hardhat version

--- hardhat.config.js
+++ hardhat.config.js
@@ -95,7 +95,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.7",
+                version: "0.8.17",
             },
             {
                 version: "0.4.24",
