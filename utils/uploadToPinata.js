b6001b280eb8ac48dd92c110f349e8b23aa79f26
Carlos Calderon <cdcalderon@gmail.com>
2022-11-12 16:40:50 -0600

    read files correctly

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,11 +1,26 @@
-const pinataSDK = require("@pinata/sdk")
+//const pinataSDK = require("@pinata/sdk")
+const fs = require("fs")
+const path = require("path")
 
-const pinataApiKey = process.env.PINATA_API_KEY || ""
-const pinataApiSecret = process.env.PINATA_API_SECRET || ""
-const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+// const pinataApiKey = process.env.PINATA_API_KEY || ""
+// const pinataApiSecret = process.env.PINATA_API_SECRET || ""
+// const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+const imagesLocation = "./images/nfts/"
 
-async function storeImages(imagesFilePath) {}
+async function storeImages(imagesLocation) {
+    console.log(imagesLocation)
+    const fullImagesPath = path.resolve(__dirname, imagesLocation)
+    console.log("fullpath ", fullImagesPath)
+    const files = fs.readdirSync(fullImagesPath)
+    console.log(files)
+    console.log("hello")
+}
 
 async function storeTokenUriMetadata(metadata) {}
 
-module.exports = { storeImages, storeTokenUriMetadata }
+storeImages(imagesLocation)
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
