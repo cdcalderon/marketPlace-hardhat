9fc915c1111a935609afb38a064026a6897096e3
Carlos Calderon <cdcalderon@gmail.com>
2022-11-20 20:19:35 -0600

    add save metadata function

--- scripts/art_generator/createImage.js
+++ scripts/art_generator/createImage.js
@@ -1,12 +1,36 @@
 const config = require("../settings/config.json")
 const path = require("path")
 const fs = require("fs")
+const crypto = require("crypto")
 
 // Define Paths
 const base = process.cwd()
 const buildBasePath = path.join(base, "/build")
+const layersBasePath = path.join(base, "/layers")
 
-const initialize = () => {}
+// Define Rarity
+let rarity
+
+// Define Helpers
+const { getAttributes, loadAttribute } = require("../helpers/attributes")
+const { createMetadata, defineAttributes } = require("../helpers/metadata")
+
+const initialize = () => {
+    try {
+        // Determine if rarity.json exists
+        rarity = require("../settings/rarity.json")
+    } catch (error) {
+        throw new Error("Missing rarity.json file")
+    }
+
+    if (fs.existsSync(buildBasePath)) {
+        fs.rmdirSync(buildBasePath, { recursive: true })
+    }
+
+    fs.mkdirSync(buildBasePath)
+    fs.mkdirSync(path.join(buildBasePath, "/json"))
+    fs.mkdirSync(path.join(buildBasePath, "/images"))
+}
 
 // Define Canvas
 
@@ -15,13 +39,32 @@ const canvas = createCanvas(config.image_details.width, config.image_details.hei
 const ctx = canvas.getContext("2d")
 
 // -- Create on the canvas
-const createImage = async () => {}
+const createImage = async () => {
+    let imageCount = 1
+    let imagesFailed = 0
+    let AttributesLoaded = []
+    let layerNames = []
+    let layersPath = ""
+    let imageHashes = []
+
+    // TODO: Need a way to break out if image_count is higher than what can be generated...
+    //  Pending ...
+}
 
 // Save the image
 const saveImage = (_imageCount) => {
     console.log(`Saving Image...\n`)
 
     fs.writeFileSync(`${buildBasePath}/images/${_imageCount}.png`, canvas.toBuffer("image/png"))
+}
+
+// Save the metadata
+const saveMetadata = (_metadata, _imageCount) => {
+    fs.writeFileSync(
+        `${buildBasePath}/json/${_imageCount}.json`,
+        JSON.stringify(_metadata, null, 2)
+    )
+    console.log(_metadata, _imageCount)
 }
 
 const main = () => {
