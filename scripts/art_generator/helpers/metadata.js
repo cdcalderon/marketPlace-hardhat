3886f5bc1ecefc17765810277b95a58c40fd5be5
Carlos Calderon <cdcalderon@gmail.com>
2022-11-24 20:29:46 -0600

    add settings and config + deps

--- scripts/art_generator/helpers/metadata.js
+++ scripts/art_generator/helpers/metadata.js
@@ -1,7 +1,7 @@
-const config = require('../settings/config.json')
+const config = require("../settings/config.json")
 
 const createMetadata = (_hash, _count, _attributes) => {
-    let dateTime = Date.now();
+    let dateTime = Date.now()
 
     let metadata = {
         id: _hash,
@@ -22,7 +22,7 @@ const defineAttributes = (_names) => {
 
     for (var i = 0; i < config.layers.length; i++) {
         attributes.trait_type = config.layers[i]
-        attributes.value = _names[i].replace('.png', "")
+        attributes.value = _names[i].replace(".png", "")
 
         features.push(attributes)
 
@@ -34,5 +34,5 @@ const defineAttributes = (_names) => {
 
 module.exports = {
     createMetadata,
-    defineAttributes
+    defineAttributes,
 }
